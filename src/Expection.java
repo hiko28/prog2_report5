97d9064be4a6d3e478e8484fa1b57b88c859b3e0
Gennan <e195735@ie.u-ryukyu.ac.jp>
2021-02-24 07:37:20 +0900

    report5　step3

--- src/Expection.java
+++ src/Expection.java
@@ -1,7 +1,24 @@
 public class Expection {
-    public static void main(String[] args) {
-        String str = null;
-        int num = str.length();
-        System.out.println(num);
+    public static void main (String[]args){
+
+        String str = "百二十三";
+        int value =  Integer.parseInt(str);
+
+
+
+
+
+        /*ステップ1, 2にて使用
+        try {
+            String str = null;
+            int num = str.length();
+            System.out.println(num);
+        }
+        catch (NullPointerException ex){
+            System.out.println("NullPointerExceptionの発生");
+            System.out.println(ex.getMessage());
+        }
+
+         */
     }
 }
